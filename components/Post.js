211b50869bc6c10a95b79e56d2785063a9b70a4a
akataz <tyrus.brenc@gmail.com>
2017-04-25 00:04:12 -0400

    refactor api call to util file for export

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,21 @@
+//import { Image } from 'react';
 
-
-const Post = ({props}) => {
-  <p>{props}</p>
+const Post = (props) => {
+  const {
+    tags,
+    title,
+    date,
+    readMore,
+    pic,
+    body
+  } = this.props;
+  <div>
+    <div>{title}</div>
+    <div>{tags}</div>
+    <div>{date}</div>
+    <img uri={pic}/>
+    <div>{body}
+  </div>
 }
 
 export default Post;
--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
 import Link from 'next/link';
-import getPosts from '../lib/getPosts'
 
 export default () => (
   <div>
     <div>Is this thing on? <Link href='/about'><a style={{ fontSize: 50 }}>About</a></Link></div>
     <div>Probably time to go home <Link href='/index'><a>Home</a></Link></div>
+    <div>This site was made using Next.js which is a sweet library that bundles React, routing, Hot Module Loading, Babel Transpiling, and
+      probaly a whole host of other cool stuff that I haven't discovered yet.  Deployed using Now, a sweet deployment solution created by the same
+      awesome folks who created Next.js</div>
   </div>
 )
